d8321d40d1a1ddbd47e7ca4fa530a268a2f64eeb
Kim deok hyeon <kdhmny@naver.com>
2022-03-01 14:39:42 +0900

    초기 구조

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: constant_identifier_names
+
+const String CACHED_TOKEN = 'CACHED_TOKEN';
+
+const String API_BASE_URL = 'BASE_URL'; // add the base url here
+const String LOGIN_USER = 'tokens';
+const String CREATE_USER = 'create';
+
+// 에러메세지
+const String NO_CONNECTION_ERROR = 'You are not connected to the Internet';
+
+const double DEFAULT_PAGE_PADDING = 20;
+
+//routes
+const String HOME_ROUTE = '/';
+const String SETTING_ROUTE = '/change_password';
+const String DEVELOPER_INTRO_ROUTE = '/change_password';
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:oceanview/core/utils/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
-  // This widget is the root of your application.
+  MyApp({Key? key}) : super(key: key);
+
+  NavigatorState get navigator => _navigatorKey.currentState!;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        navigatorKey: _navigatorKey,
+        builder: (context, child) {
+          return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+              child: child!);
+        },
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          DefaultWidgetsLocalizations.delegate
+        ],
+        supportedLocales: const [
+          Locale('ko', 'KR'),
+          Locale('en', 'US')
+        ], //, 'KR')],
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        home: const MyHomePage(title: 'init'));
   }
 }
 
